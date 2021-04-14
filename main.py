2dc0a36fd35b50d02e5334fd41765c1459c8c323
sudo180272 <sudo180272@protonmail.ch>
2021-04-14 20:31:27 +0900

    main: complete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-import mathplotlib
 
-for i in range(1, 100, 10):
-    print(i / 2 * math.sin(2 * math.pi / i))
+limit = int(input('limit: '))
+steps = int(input('steps: '))
+
+for n in range(1, limit + 1, steps):
+    print(f'n = {n}, {n / 2 * math.sin(2 * math.pi / n)}')
 
