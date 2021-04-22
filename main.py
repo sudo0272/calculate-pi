7734d7115062869f251e4bf61bb28a875f179e27
sudo180272 <sudo180272@protonmail.ch>
2021-04-23 00:02:53 +0900

    main: implement second method

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
+import mpmath as mp
+import decimal
 import math
+import random
 
-n = 1
+method = int(input('Method(1 ~ 2): '))
+precision = int(input('Precision: '))
 
-while True:
-    try:
-        print(f'\rn = {n}, {n / 2 * math.sin(2 * math.pi / n)}', end='')
+if method == 1:
+    mp.dps = 40
 
-        n += 1
+    n = 1
+    while True:
+        try:
+            print(f'\rn = {n}, {mp.mpf(n) / mp.mpf(2) * mp.sin(2 * mp.pi / mp.mpf(n))}', end='')
 
-    except KeyboardInterrupt:
-        break
+            n += 1
+
+        except KeyboardInterrupt:
+            break
+
+elif method == 2:
+    decimal.getcontext().prec = precision
+
+    circleDots = 0
+    squareDots = 0
+
+    while True:
+        try:
+            x = random.random() * 10 % 1
+            y = random.random() * 10 % 1
+
+            # if the dot is in the circle
+            if x ** 2 + y ** 2 <= 1:
+                circleDots += 1
+
+            squareDots += 1
+
+            print(f'\rN = {squareDots}, M = {circleDots}, ', end='')
+            print(4 * circleDots / decimal.Decimal(squareDots), end='')
+
+        except KeyboardInterrupt:
+            break
 
 print()
 
